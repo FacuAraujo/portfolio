46c3f2e2d2e32ab2119815cbdf6f9b2b1e3caec8
Facu <facuaaraujo@gmail.com>
2020-04-08 19:42:39 -0300

    Cree las rutas en el componente App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import About from './pages/About';
 
 import './App.scss';
 
-function App() {
-  return <Home />;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/sobre-mi" component={About} />
+        <Route path="*" />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
