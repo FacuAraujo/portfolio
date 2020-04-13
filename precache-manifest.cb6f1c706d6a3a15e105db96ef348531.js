c7420029d7d6534745ca7ab874b1e2afa0405e99
Facundo Araujo <facuaaraujo@gmail.com>
2020-04-13 18:43:37 -0300

    Updates

---
+++ precache-manifest.cb6f1c706d6a3a15e105db96ef348531.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1da94cb2421355b1873527432fd98f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfb1890aa74499a4d440",
+    "url": "/static/css/main.591c4d07.chunk.css"
+  },
+  {
+    "revision": "3ec72cf53d4b1eecc293",
+    "url": "/static/js/2.df19abde.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.df19abde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfb1890aa74499a4d440",
+    "url": "/static/js/main.6604a853.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  },
+  {
+    "revision": "9e60cefc85da88bb30a5f1ebe47a9159",
+    "url": "/static/media/arrow.9e60cefc.svg"
+  },
+  {
+    "revision": "e42722c718f43af4e65217acf896b3d8",
+    "url": "/static/media/left-bottom.e42722c7.jpg"
+  },
+  {
+    "revision": "b293cb25ce07a677c0041697e176dd38",
+    "url": "/static/media/left-top.b293cb25.jpg"
+  },
+  {
+    "revision": "dae7a87a4e29a58e903c4801a6406c68",
+    "url": "/static/media/right-bottom.dae7a87a.jpg"
+  },
+  {
+    "revision": "89348fbc80399627ba79d1b7883d99bc",
+    "url": "/static/media/right-top.89348fbc.jpg"
+  }
+]);
